9d6f9f7a753b3ee30e2d6755a6e6d9b0a6f093c0
Kiragu12 <macharia.kiragu@strathmore.edu>
2025-05-11 21:40:00 +0300

    trial commmit

--- hello.java
+++ hello.java
@@ -1,4 +1,11 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Welcome to my first GitHub Java project!");
 
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
 
-    System.out.println(age);
-
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+    }
+}
