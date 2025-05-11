689cbadcd7d7bfc2376dc439f14172eed9bfe175
Kiragu12 <macharia.kiragu@strathmore.edu>
2025-05-11 21:41:06 +0300

    trial commmit 35

--- hello.java
+++ hello.java
@@ -4,8 +4,8 @@ public class hello {
 
         int a = 10;
         int b = 20;
-        int sum = a + b;
 
-        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+
+        System.out.println("Sum of " + a + " and " + b + " is: " );
     }
 }
